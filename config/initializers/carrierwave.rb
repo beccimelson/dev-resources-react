e0cd83696abf2508e4d5a85219003f32416b38b4
Becci Melson <beccimelson@users.noreply.github.com>
2018-03-08 00:10:06 -0500

    Delete file

--- config/initializers/carrierwave.rb
+++
@@ -1,9 +0,0 @@
-CarrierWave.configure do |config|
-    config.fog_provider = 'fog/aws'                        # required
-    config.fog_credentials = {
-      provider:              'AWS',                        # required
-      aws_access_key_id:     'AKIAIBXOX5MNTNA34YYA',                        # required
-      aws_secret_access_key: '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',                        # required
-    }
-    config.fog_directory  = 'superhi-tutorial-development'                                   # required
-  end
